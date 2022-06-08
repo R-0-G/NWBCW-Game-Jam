bceff7cdb343058631d7ed1e0cb9f678e282b2f9
R_0_G <owentexting@gmail.com>
2022-06-08 04:11:51 +0100

    working 2d tileset

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Human : MonoBehaviour
+{
+	public NavMeshAgent agent;
+	public Transform[] targets;
+
+	private Transform target;
+	private Vector3 trgPos;
+
+	private void Start()
+	{
+		InvokeRepeating("FindNextTarget", 1f, 5f);
+	}
+
+	private void FindNextTarget()
+	{
+		target = targets[Random.Range(0, targets.Length - 1)];
+		trgPos = target.position;
+
+		agent.destination = trgPos;
+		agent.speed = 3f;
+	}
+}
