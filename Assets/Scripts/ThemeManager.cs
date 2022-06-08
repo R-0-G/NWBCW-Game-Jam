a745d3e7701884f98337c2da8c7b0770715774b0
R_0_G <owentexting@gmail.com>
2022-06-08 02:57:41 +0100

    Update unity editor version

---
+++ Assets/Scripts/ThemeManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ThemeManager : ScriptableObject
+{
+	public DirectionalTexture2D environment;
+
+}
